2578bd8385bb446a2bc8cb49ff113b0c605b446b
Jose Ignacio Retamal <peperetamalthomsen@outlook.com>
2019-10-13 12:02:43 +0100

    code format

--- display/mnistDisplay.py
+++ display/mnistDisplay.py
@@ -11,28 +11,21 @@ w = Canvas(master,
            height=canvas_height)
 w.pack()
 
-
 x1, y1, x2, y2 = 0, 0, 10, 10
 #x2, y2 = 10, 10
 
 reader = r.Reader('../mnist/train-labels-idx1-ubyte.gz',
                   '../mnist/train-images-idx3-ubyte.gz')
 
-
 image = [[0 for x in range(28)]
          for y in range(28)]
 reader.getImageArray2D1(5678, image)
-
-# print(image)
-
-# image = reader.getArray()
 
 for k in range(0, 28):
     for j in range(0, 28):
         number = image[k][j]
         color = "#" + str(hex(number)[2:5]) + \
             str(hex(number)[2:5]) + str(hex(number)[2:5])
-
         w.create_rectangle(x1, y1, x2, y2, fill=color)
         x1, x2 = x1 + 10, x2 + 10
     x1, y1, x2, y2 = 0, y1 + 10, 10, y2 + 10
