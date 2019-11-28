de656edfca6c9b01f132abb9c91058a8a19ae0db
Pepe <joseretamal@outlook.ie>
2019-11-28 12:10:50 +0000

    Model training : readind mnist done

--- webapp/app.py
+++ webapp/app.py
@@ -208,54 +208,7 @@ def process():
             pos +=1
         print(num)
     
-        #print(f)
-        # a = np.asarray(img)
-        # n = []
-        # for number in a:
-        #     for j in number:
-        #         n.append(j[1])
-        # n = np.array(n)
-        # n = n.reshape(20,20)
-        # plt.imshow(n, cmap='gray')
-        # plt.show()
-        # img.save("test.png")
-        # mask = n>0
-        # n =a[np.ix_(mask.any(1),mask.any(0))]
-        # print(n)
-        #Image.fromarray(a,"RGB").show()
-       # print(image)
-    #     c = 0
-    #     for i in image:
-    #         image[c] = 255-i
-    #         c += 1
-    #     print(image)
-    #     image = np.array(image)
-    #     image = image.reshape(1,28,28,1)
-    #     image = image.astype('float32')
-    #     image /= 255
-    #     count =0
-        
-      
-    #     #image = image.reshape(28,28)
-    #     #finalImg = finalImg.astype('float32')
-    #   # print(image)
-    #     finalImg = image
-       
-
-    #     # reshape image
-    #     #finalImg  = ~np.array(list(image)).reshape(1, 784).astype(np.uint8) / 255.0
-    #     #finalImg = np.reshape(1, 784)
-
-       
-  
-
-
-        # with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-        #     future = executor.submit(make_prediction,finalImg)
-        #     print(future.result())
-
-    return f'Hello, !'
-
+    return str(num)
 
 def make_prediction(img):
     model = load_model('../models/cnn.h5')
